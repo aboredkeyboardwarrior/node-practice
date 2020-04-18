6de74cea6b387f2e9ee6d1fb00a73d213f04e33a
naws <aboredkeyboardwarrior@gmail.com>
2020-04-19 03:23:21 +0800

    added pipe

--- firstServer/app.js
+++ firstServer/app.js
@@ -2,11 +2,16 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+ 
+myReadStream.pipe(myWriteStream);
 
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received');
-    console.log(chunk);
-});
+
+// // manual way to pipe
+// myReadStream.on('data', function(chunk){
+//     console.log('new chunk received');
+//     console.log(chunk);
+// });
 
 
 // var server = http.createServer(function(req, res){
