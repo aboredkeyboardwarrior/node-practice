77a64c3da66f86cfe44e6fb0b76ad59d13150dc9
naws <aboredkeyboardwarrior@gmail.com>
2020-04-19 03:25:35 +0800

    piped to response

--- firstServer/app.js
+++ firstServer/app.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
+
  
 myReadStream.pipe(myWriteStream);
 
@@ -14,11 +14,12 @@ myReadStream.pipe(myWriteStream);
 // });
 
 
-// var server = http.createServer(function(req, res){
-//     console.log(`request was made to ${req.url}`);
-//     res.writeHead(200, {'Content-Type': 'text/plain'}); //status, content type
-//     res.end('hello, world') 
-// });
+var server = http.createServer(function(req, res){
+    console.log(`request was made to ${req.url}`);
+    res.writeHead(200, {'Content-Type': 'text/plain'}); //status, content type
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    myReadStream.pipe(res);
+});
 
-// server.listen(3000, '127.0.0.1');  // port, ip address
-// console.log('listening to port 3000')
+server.listen(3000, '127.0.0.1');  // port, ip address
+console.log('listening to port 3000')
