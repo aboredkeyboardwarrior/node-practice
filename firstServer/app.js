759f0aa70a76432862e33ac4155afc6b0998244d
naws <aboredkeyboardwarrior@gmail.com>
2020-04-19 19:19:01 +0800

    now return json file

--- firstServer/app.js
+++ firstServer/app.js
@@ -11,9 +11,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log(`request was made to ${req.url}`);
-    res.writeHead(200, {'Content-Type': 'text/html'}); //status, content type - text, html
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'}); //status, content type - text, html
+    var myObj = {
+        name: 'ryu',
+        job: 'Ninja',
+        age: 29
+    };
+    res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');  // port, ip address
