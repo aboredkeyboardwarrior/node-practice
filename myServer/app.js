0c02a96e3d56e8bd3a94f82e8f4b5c86be16804e
naws <aboredkeyboardwarrior@gmail.com>
2020-04-21 18:08:56 +0800

    Added sample code, renamed files

--- myServer/app.js
+++ myServer/app.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets')); //request to /assets/* searches for file called * in assets folder
 
+// create application/json parser
+var jsonParser = bodyParser.json();
 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function(req, res){
     res.render('index');
@@ -16,6 +22,14 @@ app.get('/profile/:id', function(req, res){
     var fakeData = {age: 29, job: 'ninja', letters:['a', 'b', 'c']};
     res.render('profile', {person: req.params.id, data:fakeData});
 });
+
+// POST /login gets urlencoded bodies
+app.post('/contact', urlencodedParser, function (req, res){
+    console.log('post req received');
+    console.log(req.body);
+    res.render('contact-success', {data:req.body});
+});
+
 
 // // using url parameters without view engine
 // app.get('/profile/:id', function(req, res){
